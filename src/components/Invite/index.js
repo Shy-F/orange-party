98adfe3e1c2e64d1adeff1b9e3421739c76c9dae
Shy <fleetwoodshybreasia@gmail.com>
2022-09-25 18:08:20 -0400

    decor

--- src/components/Invite/index.js
+++ src/components/Invite/index.js
@@ -22,6 +22,26 @@ const Invite = () => {
                 <div className='circle8'></div>
                 <div className='circle9'></div>
                 <div className='circle10'></div>
+                <div className='circle11'></div>
+                <div className='circle12'></div>
+                <div className='circle13'></div>
+                <div className='circle14'></div>
+                <div className='circle15'></div>
+                <div className='circle16'></div>
+                <div className='circle17'></div>
+                <div className='circle18'></div>
+                <div className='circle19'></div>
+                <div className='circle20'></div>
+                <div className='circle21'></div>
+                <div className='circle22'></div>
+                <div className='circle23'></div>
+                <div className='circle24'></div>
+                <div className='circle25'></div>
+                <div className='circle26'></div>
+                <div className='circle27'></div>
+                <div className='circle28'></div>
+                <div className='circle29'></div>
+                <div className='circle30'></div>
                 <div className='header'>
                     <h1> You're Invited!</h1>
                 </div>
